d60299b0190882dfbbbdb9c8b317d10a1b324f16
masimof <masimofrances@gmail.com>
2023-05-31 10:10:55 +0000

    Report

--- helpdesk_miguelangelsimo/__manifest__.py
+++ helpdesk_miguelangelsimo/__manifest__.py
@@ -38,7 +38,8 @@
         "views/helpdesk_ticket_views.xml",
         "views/helpdesk_ticket_tag_views.xml",
         "data/helpdesk_cron.xml",
-
+        "report/ir_actions_report_templates.xml",
+        "report/ir_actions_report.xml",
         
     ],
  #   "demo": [
